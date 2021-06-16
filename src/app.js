103816aee3fded37ae3705cb8b897106d0a5ef7e
David Hernandez Arias <david.hernandez3@coxautoinc.com>
2021-06-16 13:42:22 -0400

    updating help and about page

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
 	res.render("help", {
-		message: "This is my message!",
+		message: "Let me in!!!",
 		title: "Help",
 		name: "David Hernandez"
 	});
