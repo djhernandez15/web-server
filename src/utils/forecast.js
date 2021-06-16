cf74d7b40b1516a79f8af7dc681c0e743dafb8aa
David Hernandez Arias <david.hernandez3@coxautoinc.com>
2021-06-16 13:49:24 -0400

    tweaking msg

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,6 +4,7 @@ const forecast = (latitude, longitude, callback) => {
 	const url = `http://api.weatherstack.com/current?access_key=b8e7199d52fe2072a6d4b0b22a27ebd5&query=${longitude},${latitude}&units=f`;
 
 	request({ url, json: true }, (error, {body}) => {
+		console.log(body.current)
 		if (error) {
 			callback("Unable to connect to location services!");
 		} else if (body.error) {
@@ -11,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
 		} else {
 			callback(
 				undefined,
-				`It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+				`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`
 			);
 		}
 	});
